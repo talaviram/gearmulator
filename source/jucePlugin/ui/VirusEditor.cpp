3332c789c6260c533c5df0b40085be1ae8029626
Tal Aviram <me@talaviram.com>
2021-07-13 01:02:16 +0300

    ui - add main (pages) buttons.

--- source/jucePlugin/ui/VirusEditor.cpp
+++ source/jucePlugin/ui/VirusEditor.cpp
@@ -12,6 +12,7 @@ VirusEditor::VirusEditor()
 
     m_background->setBufferedToImage (true);
     addAndMakeVisible (*m_background);
+    addAndMakeVisible (m_mainButtons);
 
     setSize (kPanelWidth, kPanelHeight);
 }
@@ -19,4 +20,30 @@ VirusEditor::VirusEditor()
 void VirusEditor::resized()
 {
     m_background->setBounds (getLocalBounds());
+    m_mainButtons.setBounds (394, 106, m_mainButtons.getWidth(), m_mainButtons.getHeight());
+}
+
+VirusEditor::MainButtons::MainButtons()
+: m_oscFilter ("OSC|FILTER", DrawableButton::ImageRaw)
+, m_lfoMatrix ("LFO|MATRIX", DrawableButton::ImageRaw)
+, m_effects ("EFFECTS", DrawableButton::ImageRaw)
+, m_arpSettings ("ARP|SETTINGS", DrawableButton::ImageRaw)
+{
+    constexpr auto numOfMainButtons = 4;
+    setupButton (0, Drawable::createFromImageData (BinaryData::GLOBAL_btn_osc_filter_141x26_png, BinaryData::GLOBAL_btn_osc_filter_141x26_pngSize), m_oscFilter);
+    setupButton (1, Drawable::createFromImageData (BinaryData::GLOBAL_btn_lfo_matrix_141x26_png, BinaryData::GLOBAL_btn_lfo_matrix_141x26_pngSize), m_lfoMatrix);
+    setupButton (2, Drawable::createFromImageData (BinaryData::GLOBAL_btn_effects_141x26_png, BinaryData::GLOBAL_btn_effects_141x26_pngSize), m_effects);
+    setupButton (3, Drawable::createFromImageData (BinaryData::GLOBAL_btn_arp_settings_141x26_png, BinaryData::GLOBAL_btn_arp_settings_141x26_pngSize), m_arpSettings);
+    setSize ((kButtonWidth + kMargin) * numOfMainButtons, kButtonHeight);
+}
+
+void VirusEditor::MainButtons::setupButton (int i, std::unique_ptr<Drawable>&& btnImage, juce::DrawableButton& btn)
+{
+    auto onImage = btnImage->createCopy();
+    onImage->setOriginWithOriginalSize({0, -kButtonHeight});
+    btn.setClickingTogglesState (true);
+    btn.setRadioGroupId (kGroupId);
+    btn.setImages (btnImage.get(), nullptr, nullptr, nullptr, onImage.get());
+    btn.setBounds ((i > 1 ? -1 : 0) + i * (kButtonWidth + kMargin), 0, kButtonWidth, kButtonHeight);
+    addAndMakeVisible (btn);
 }
--- source/jucePlugin/ui/VirusEditor.h
+++ source/jucePlugin/ui/VirusEditor.h
@@ -8,5 +8,16 @@ public:
     VirusEditor();
     void resized() override;
 private:
+    struct MainButtons : juce::Component
+    {
+        MainButtons();
+        void setupButton (int i, std::unique_ptr<juce::Drawable>&& btn, juce::DrawableButton&);
+        juce::DrawableButton m_oscFilter, m_lfoMatrix, m_effects, m_arpSettings;
+        static constexpr auto kMargin = 5;
+        static constexpr auto kButtonWidth = 141;
+        static constexpr auto kButtonHeight = 26;
+        static constexpr auto kGroupId = 0x3FBBA;
+    } m_mainButtons;
+
     std::unique_ptr<juce::Drawable> m_background;
 };
