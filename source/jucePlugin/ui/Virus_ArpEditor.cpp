2957b29eb23042d09fc1a7e43d4f23fe2880d235
Tal Aviram <me@talaviram.com>
2021-07-17 10:08:22 +0300

    ui - arp (knobs only)

--- source/jucePlugin/ui/Virus_ArpEditor.cpp
+++ source/jucePlugin/ui/Virus_ArpEditor.cpp
@@ -8,4 +8,57 @@ ArpEditor::ArpEditor()
 {
     setupBackground(*this, m_background, BinaryData::bg_arp_1018x620_png, BinaryData::bg_arp_1018x620_pngSize);
     setBounds(m_background->getDrawableBounds().toNearestIntEdges());
+
+    m_velocityAmount.setBounds(23, 28, 439, 238);
+    addAndMakeVisible(m_velocityAmount);
+    m_inputs.setBounds(23, m_velocityAmount.getBottom() + 2, 439, 118);
+    addAndMakeVisible(m_inputs);
+    m_arp.setBounds(m_velocityAmount.getRight() + 2, 28, 552, 120);
+    addAndMakeVisible(m_arp);
+    m_softKnobs.setBounds(m_arp.getX(), m_arp.getBottom() + 2, 552, 116);
+    addAndMakeVisible(m_softKnobs);
+    m_patchSettings.setBounds(m_softKnobs.getX(), m_softKnobs.getBottom() + 2, 552, 194);
+    addAndMakeVisible(m_patchSettings);
+}
+
+ArpEditor::VelocityAmount::VelocityAmount()
+{
+    constexpr auto y = 19;
+    for (auto *s : {&m_osc1Shape, &m_filter1Freq, &m_filter1Res, &m_pulseWidth, &m_volume, &m_panorama, &m_osc2Shape,
+                    &m_filter2Freq, &m_filter2Res, &m_fmAmount})
+        setupRotary(*this, *s);
+    m_osc1Shape.setBounds(31, y, knobSize, knobSize);
+    m_filter1Freq.setBounds(m_osc1Shape.getRight() - 7, y, knobSize, knobSize);
+    m_filter1Res.setBounds(m_filter1Freq.getRight() - 8, y, knobSize, knobSize);
+    m_pulseWidth.setBounds(m_filter1Res.getRight() - 7, y, knobSize, knobSize);
+    m_volume.setBounds(m_pulseWidth.getRight() - 3, y, knobSize, knobSize);
+    m_panorama.setBounds(m_volume.getRight() - 9, y, knobSize, knobSize);
+
+    const auto y2 = m_osc1Shape.getBottom() + y;
+    m_osc2Shape.setBounds(31, y2, knobSize, knobSize);
+    m_filter2Freq.setBounds(m_osc1Shape.getRight() - 7, y2, knobSize, knobSize);
+    m_filter2Res.setBounds(m_filter1Freq.getRight() - 8, y2, knobSize, knobSize);
+    m_fmAmount.setBounds(m_filter1Res.getRight() - 7, y2, knobSize, knobSize);
+}
+
+ArpEditor::Arpeggiator::Arpeggiator()
+{
+    constexpr auto y = 18;
+    for (auto *s : {&m_globalTempo, &m_noteLength, &m_noteSwing})
+        setupRotary(*this, *s);
+    m_globalTempo.setBounds(341, y, knobSize, knobSize);
+    m_noteLength.setBounds(m_globalTempo.getRight() - 8, y, knobSize, knobSize);
+    m_noteSwing.setBounds(m_noteLength.getRight() - 7, y, knobSize, knobSize);
+}
+
+ArpEditor::PatchSettings::PatchSettings()
+{
+    constexpr auto y = 18;
+    for (auto *s : {&m_patchVolume, &m_panning, &m_outputBalance, &m_transpose})
+        setupRotary(*this, *s);
+    m_patchVolume.setBounds(101, y, knobSize, knobSize);
+    m_panning.setBounds(m_patchVolume.getRight() - 9, y, knobSize, knobSize);
+    const auto y2 = m_patchVolume.getBottom() + 9;
+    m_outputBalance.setBounds(m_patchVolume.getX(), y2, knobSize, knobSize);
+    m_transpose.setBounds(m_panning.getX(), y2, knobSize, knobSize);
 }
--- source/jucePlugin/ui/Virus_ArpEditor.h
+++ source/jucePlugin/ui/Virus_ArpEditor.h
@@ -8,5 +8,45 @@ public:
     ArpEditor();
 
 private:
+    struct VelocityAmount : juce::Component
+    {
+        VelocityAmount();
+        juce::Slider m_osc1Shape;
+        juce::Slider m_filter1Freq;
+        juce::Slider m_filter1Res;
+        juce::Slider m_pulseWidth;
+        juce::Slider m_volume;
+        juce::Slider m_panorama;
+        juce::Slider m_osc2Shape;
+        juce::Slider m_filter2Freq;
+        juce::Slider m_filter2Res;
+        juce::Slider m_fmAmount;
+    } m_velocityAmount;
+
+    struct Inputs : juce::Component
+    {
+    } m_inputs;
+
+    struct Arpeggiator : juce::Component
+    {
+        Arpeggiator();
+        juce::Slider m_globalTempo;
+        juce::Slider m_noteLength;
+        juce::Slider m_noteSwing;
+    } m_arp;
+
+    struct SoftKnobs : juce::Component
+    {
+    } m_softKnobs;
+
+    struct PatchSettings : juce::Component
+    {
+        PatchSettings();
+        juce::Slider m_patchVolume;
+        juce::Slider m_panning;
+        juce::Slider m_outputBalance;
+        juce::Slider m_transpose;
+    } m_patchSettings;
+
     std::unique_ptr<juce::Drawable> m_background;
 };
